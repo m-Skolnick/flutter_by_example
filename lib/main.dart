2a2c8b9671f84b27e9961ef99c79b033442dbf9b
Micaiah Skolnick <mskolnick@coolfiresolutions.com>
2020-02-04 11:48:45 -0500

    add GlobalClass

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,49 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Example Global Variable"),
+        ),
+        body: SomePage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //   String _connectionStatus = 'Unknown';
-  // final Connectivity _connectivity = Connectivity();
-  // StreamSubscription<ConnectivityResult> _connectivitySubscription;
-
-  @override
-  void initState() {
-    super.initState();
-    // _connectivitySubscription =
-    //     _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-  }
-
+class SomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    GlobalClass();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Example app"),
-      ),
-      body: Center(
-        child: Text("test text"),
+    //Here is where your page context is stored in the global class.
+    GlobalClass.shared.context = context;
+    return Center(
+      child: RaisedButton(
+        child: Text("Show a Toast"),
+        onPressed: () {
+          _showToast();
+        },
       ),
     );
   }
+}
+
+_showToast() {
+  Scaffold.of(GlobalClass.shared.context).showSnackBar(SnackBar(
+    content: Text("Here is your toast"),
+  ));
 }
 
 class GlobalClass {
-  // Singleton for global auth access
+  // Singleton for global MaterialApp context access
   static final GlobalClass _singleton = GlobalClass._internal();
   GlobalClass._internal();
   factory GlobalClass() => _singleton;
   static GlobalClass get shared => _singleton;
 
-  var globalDataStream = StreamController<String>.broadcast();
-
-  broadcast(String newString) {
-    globalDataStream.sink.add(newString);
-  }
-
-  void dispose() {
-    globalDataStream.close();
-  }
+  BuildContext context;
 }
